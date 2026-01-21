47131b47ba5c4ac3a17b5202d6abc822c98c7449
Gawc1uuu <gawlikjakub9@gmail.com>
2026-01-21 17:25:52 +0100

    add database package

--- packages/database/src/queries/users.ts
+++ packages/database/src/queries/users.ts
@@ -1,5 +1,5 @@
-import { users } from "../schema/schema";
-import type { DbExecutor } from "../types";
+import { users } from '../schema/schema';
+import type { DbExecutor } from '../types';
 
 export const getUsers = async (db: DbExecutor) => {
   const data = await db.select().from(users);
